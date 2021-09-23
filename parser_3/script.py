f4137f613c0406b4b88531733146a441912da681
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-09-23 12:03:50 +0300

    parser 3 - 2nd step - data to json

--- parser_3/script.py
+++ parser_3/script.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import lxml
 import os
 import time
+import json
+from datetime import datetime
 
 
 def get_all_pages():
@@ -27,7 +29,6 @@ def get_all_pages():
 
     for i in range(1, pages_count + 1):
         url = f"https://shop.casio.ru/catalog/filter/gender-is-male/apply/?PAGEN_1={i}"
-        print(url)
 
         r = requests.get(url=url, headers=headers)
 
@@ -38,11 +39,43 @@ def get_all_pages():
 
     return pages_count + 1
 
+def collect_data(pages_count):
+    cur_date = datetime.now().strftime("%d_%m_%Y")
+
+    data = []
+    for page in range(1, pages_count):
+        with open(f"data/page_{page}.html", encoding="utf-8") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+        item_cards = soup.find_all("a", class_="product-item__link")
+
+        for item in item_cards:
+            product_article = item.find("p", class_="product-item__articul").text.strip()
+            product_price = item.find("p", class_="product-item__price").text.lstrip("руб. ")
+            product_url = f'https://shop.casio.ru{item.get("href")}'
+
+            #print(f"Article: {product_article} - Price: {product_price} - URL: {product_url}")
+
+            data.append(
+                {
+                    "product_article": product_article,
+                    "product_price": product_price,
+                    "product_url": product_url
+                }
+            )
+
+        print(f"[INFO] Successfully finished {page}")
+
+    with open(f"data_{cur_date}.json", "a", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
 
 
 
 def main():
     pages_count = get_all_pages()
+    collect_data(pages_count=pages_count)
 
 
 if __name__ == '__main__':
