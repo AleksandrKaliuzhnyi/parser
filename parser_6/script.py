77f5fcc29b10ae50134c031d06d144aef6d15fc9
Oleksandr.Kaliuzhnyi <aleksandr.kaliuzhnyi@gmail.com>
2021-09-24 17:19:39 +0300

    parser 6 - function for collecting urls

--- parser_6/script.py
+++ parser_6/script.py
@@ -37,10 +37,28 @@ def get_source_html(url):
         driver.close()
         driver.quit()
 
+def get_items_urls(file_path):
+    with open(file_path, encoding="utf-8") as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, "lxml")
+    items_divs = soup.find_all("div", class_="service-description")
+
+    urls = []
+    for item in items_divs:
+        item_url = item.find("div", class_="H3").find("a").get("href")
+        urls.append(item_url)
+
+    with open("items_urls.txt", "w", encoding="utf-8") as file:
+        for url in urls:
+            file.write(f"{url}\n")
+
+    return "[INFO] Urls collected successfully!"
+
 
 def main():
-    get_source_html(url="https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10")
-
+    #get_source_html(url="https://spb.zoon.ru/medical/?search_query_form=1&m%5B5200e522a0f302f066000055%5D=1&center%5B%5D=59.91878264665887&center%5B%5D=30.342586983263384&zoom=10")
+    print(get_items_urls(file_path="D:\Python\parser\parser_6\sourse-page.html"))
 
 
 if __name__ == '__main__':
